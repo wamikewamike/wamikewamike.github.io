31c5fad9d93efe2a5a07c4999d34ce32a72d4d92
wamikewamike <mike.2001@web.de>
2022-12-05 02:15:00 +0100

    Logo hinzugefuegt

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*
+function changeBlobboToCreepy(){
+  if (document.getElementById("blobbohd").src === "img/blobboHD.png"){
+    document.getElementById("blobbohd").src = "img/creepyblobbo.png";
+  } else {
+    document.getElementById("blobbohd").src = "img/blobboHD.png";
+  }
+}
+*/
